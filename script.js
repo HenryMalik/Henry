b04fa88e4da03e8e79cd11f9c98a885db5609a74
akmaltezzar <akmaldigitels@gmail.com>
2024-08-21 22:09:09 +0700

    belajar variable

---
+++ script.js
@@ -0,0 +1,15 @@
+let number1 = 10
+let number2 = 15
+let number3 = "nama"
+
+function Add (n1, n2, n3) {
+return n1 + n2 + n3;
+}
+
+
+
+const adnumber = (number1, number2) => {
+    console.log(number1 + number2);
+    
+}
+ adnumber(12, 14)
